84a3d0d92ca63b9db63f2cd23c0a98b65bdeba05
Navid <navid.anjum@northsouth.edu>
2022-07-12 20:55:27 +0600

    navbar done

--- components/NavLeft.jsx
+++ components/NavLeft.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Link from 'next/link'
 const NavLeft = () => {
   return (
     <div className='flex flex-col md:flex-row space-x-4 space-y-2 items-center justify-center md:space-y-0'>
-        <h1 className='text-2xl md:w-1/2 font-bold'>Rifat Bin Azad</h1>
+        <Link href="/" passHref><a className='text-2xl md:w-1/2 font-bold'>Rifat Bin Azad</a></Link>
         <h1 className="bg-fadeOrange w-[35px] h-[1px] top-1.5 block md:hidden "></h1>
         <h1>Digital Marketer</h1>
     </div>

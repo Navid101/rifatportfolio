70fb57ec7480f3d73454286426ba5bdeb96f3e27
Navid <navid.anjum@northsouth.edu>
2022-07-12 23:12:21 +0600

    index page half done

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
+import Image from "next/image";
+import Bio from "../components/Bio";
 
 
 export default function Home() {
   return (
-    <div className="container px-8 m-auto md:space-x-6">
-      <h1 className="text-green text-3xl">Hello World</h1>
+    <div className="w-full bg-darkBlack py-28">
+      <div className="container px-8 m-auto  flex justify-center items-center space-x-28">
+        <Image src="/assets/rifat.webp" width="480px" height="480px" className="rounded-full"></Image>
+        <Bio/>
+      </div>
     </div>
   )
 }
